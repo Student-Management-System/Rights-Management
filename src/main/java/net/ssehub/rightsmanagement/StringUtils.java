33f8df56dbe20816ed6dc0e57a6d170e0ac12e41
TimKuno <tim.kunold@gmx.de>
2020-04-08 17:10:36 +0200

    class to deal with illegal/invalid characters

---
+++ src/main/java/net/ssehub/rightsmanagement/StringUtils.java
@@ -0,0 +1,33 @@
+package net.ssehub.rightsmanagement;
+
+/**
+ * Manages illegal/invalid characters in the path or name.
+ * 
+ * @author Kunold
+ *
+ */
+public class StringUtils {
+
+    /**
+     * Normalizes the path.
+     * @param path that needs to be normalized.
+     * @return the normalized path.
+     */
+    public static String normalizePath(String path) {
+        StringBuffer normalizedPath = new StringBuffer();
+        
+        return normalizedPath.toString();
+    }
+    
+    /**
+     * Normalizes the name of a group.
+     * @param name that needs to be normalized.
+     * @return the normalized name.
+     */
+    public static String normalizeName(String name) {
+        StringBuffer normalizedName = new StringBuffer();
+        
+        return normalizedName.toString();
+    }
+    
+}
