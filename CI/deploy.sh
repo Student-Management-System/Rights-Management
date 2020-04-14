7123f135d11e4c02a6c6d392948cf91c058d2753
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-04-14 11:50:55 +0200

    Deploy script

---
+++ CI/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+jarFile=$(find ../target/ -name "RightsManagement*.jar")
+
+# Copy Unit & JAR
+scp -i ~/.ssh/id_rsa_student_mgmt_backend "rightsmanagement.service" elscha@147.172.178.30:~/.config/systemd/user
+scp -i ~/.ssh/id_rsa_student_mgmt_backend "${jarFile}" elscha@147.172.178.30:~/Sparky/RightsManagement.jar
+
+# Update service
+ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 'systemctl --user daemon-reload'
+ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 'systemctl --user restart rightsmanagement'
